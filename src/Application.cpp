645d3ddc5d71e7aae793fcda3240384f112f04ca
drfreeman816 <eliseuv816@gmail.com>
2018-02-04 22:16:12 -0200

    Error handling, uniforms and vertex array.

--- src/Application.cpp
+++ src/Application.cpp
@@ -10,6 +10,39 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+// Debug mode
+#define DEBUG
+
+/*      ERROR HANDLING      */
+
+// Query OpenGL for errors
+#ifdef DEBUG
+#define GLCall(x) \
+    GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+#else
+#define GLCall(x) x
+#endif
+
+#define ASSERT(x) if (!(x)) __builtin_trap();
+
+// Clear errors
+static void GLClearError(){
+    while (glGetError() != GL_NO_ERROR);
+}
+
+// Check for errors
+static bool GLLogCall(const char* function, const char* file, unsigned int line){
+    while (GLenum error = glGetError()){
+        std::cerr << "[OpenGL ERROR] (" << error << "): " << function << " in " << file << ":" << line << std::endl;
+        return false;
+    }
+    return true;
+}
+
+/*      SHADER COMPILATION      */
+
 // Shaders source codes
 struct shaderProgramSource{
     std::string vertex;
@@ -114,8 +147,17 @@ int main(void)
         return -1;
     }
 
+    // Select OpenGL version and profile
+    // OpenGL 3.3 CORE
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     // Make the window's context current
     glfwMakeContextCurrent(window);
+
+    // VSync
+    glfwSwapInterval(1);
 
     // Initialize GLEW library
     if (glewInit() != GLEW_OK)
@@ -124,31 +166,60 @@ int main(void)
     // Print OpenGL version
     std::cout << "Using OpenGL " << glGetString(GL_VERSION) << " by " << glGetString(GL_VENDOR) << std::endl;
 
+    /*      DATA      */
+    // Vertices
+    float vertices[] = {
+    //  x      y
+        -0.5f, -0.5f,   // 0
+         0.5f, -0.5f,   // 1
+         0.5f,  0.5f,   // 2
+        -0.5f,  0.5f    // 3
+    };
+    // Indices
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    /*      VERTEX ARRAY     */
+
+    // Create vertex array object
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+
+    // Bind vertex array
+    glBindVertexArray(vao);
+
     /*      VERTEX BUFFER     */
 
     // Create vertex buffer
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
+    unsigned int vbuffer;
+    glGenBuffers(1, &vbuffer);
 
     // Bind buffer
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-
-    // Data for triangle
-    float positions[6] = {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
-    };
+    glBindBuffer(GL_ARRAY_BUFFER, vbuffer);
 
     // Setup vertex attributes
     // Position
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
 
-    // Enable vertex attrbute
+    // Fill buffer with data
+    glBufferData(GL_ARRAY_BUFFER, 4*2*sizeof(unsigned int), vertices, GL_STATIC_DRAW);
+
+    // Enable vertex attribute
     glEnableVertexAttribArray(0);
 
+    /*      INDEX BUFFER     */
+
+    // Create index buffer object
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+
+    // Bind buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+
     // Fill buffer with data
-    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6*sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     /*      SHADERS     */
 
@@ -157,14 +228,29 @@ int main(void)
     unsigned int shader = createShader(shaderSource);
     glUseProgram(shader);
 
-    // Loop until the user closes the window
+    /*      Uniforms        */
+    
+    // Locate uniforms
+    int uniformLocation = glGetUniformLocation(shader, "u_color");
+    ASSERT(uniformLocation != -1);
+    
+    // Colors
+    float r = 1.0f, dr = 0.05;
+
+    /*      GAME LOOP       */
+
     while (!glfwWindowShouldClose(window))
     {
         // Render here
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Send data through uniform
+        if (r > 1.0f || r < 0.0f) dr = -dr;
+        r += dr;
+        glUniform4f(uniformLocation, r, 0.3f, 0.8f, 1.0f);
+
         // Draw call for triangles
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
